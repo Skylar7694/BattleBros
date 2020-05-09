048187b605082b5ad73d10a50040f045dec64863
Skylar Bissonnette <37849362+Skylar7694@users.noreply.github.com>
2020-05-09 16:03:19 -0500

    adding combat sequence
    combat sequence added
    -- how the combat goes and how the game handles combat

--- BattleBro/source/PlayState.hx
+++ BattleBro/source/PlayState.hx
@@ -3,6 +3,7 @@ package;
 import AssetPaths;
 import Enemy;
 import Player;
+import CombatSequence;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 import flixel.FlxState;
@@ -12,14 +13,20 @@ class PlayState extends FlxState
 	var enemy:Enemy;
 	var player:Player;
 	private var background:FlxSprite;
-	
+
 	override public function create()
 	{
+		//creates Enemy and Player objects 
 		enemy = new Enemy();
 		player = new Player();
+		combat = new CombatSequence();
+
 		super.create();
+		//creates background 
 		background=new FlxSprite();
 		background.loadGraphic(AssetPaths.BackGround__jpg);
+	
+
 		add(background);
 		add(enemy);
 		add(player);
