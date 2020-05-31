67669f11a83b1e83500cdbb324e22a4bd8c5bfae
Skylar Bissonnette <37849362+Skylar7694@users.noreply.github.com>
2020-05-31 09:17:38 -0500

    fixed screen not loading
    fix screen

--- BattleBro/source/ChoiceHud.hx
+++ BattleBro/source/ChoiceHud.hx
@@ -1,51 +1,67 @@
 package;
 
 import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.FlxSprite;
-import AssetPaths;
-import flixel.FlxState;
 import flixel.text.FlxText;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.tweens.FlxTween;
+import flixel.ui.FlxBar;
 import flixel.util.FlxColor;
 
+using flixel.util.FlxSpriteUtil;
 
-class ChoiceHud extends FlxText
+
+enum Choice
+{
+	Fight;
+    Defend;
+    Heal;
+    Flee;
+}
+class ChoiceHud extends FlxTypedGroup<FlxSprite>
 {   
-    
+    var background:FlxSprite;
     var choiceHud:ChoiceHud = new ChoiceHud();
     var choicetext:FlxText;
-    var choice:Array<String> = ["Fight", "Defend", "Heal", "Flee"];
+    var choices:Map<Choice, FlxText>;
     var i =0;
+    var screen:FlxSprite;
   public function new() 
   {
     super();
-    for(val in choice){
-        while(i< choice.length){ 
-            choicetext= new FlxText(0, 0, 0, choice[i], 32);
-            choicetext.setFormat(null,32,FlxColor.RED);
-            this.setPosition(400 + (i*10), 500+ (i*10));   
-            i++;
-            
+
+    //screen = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.TRANSPARENT);
+
+    //background = new FlxSprite().makeGraphic(120, 120, FlxColor.WHITE);
+	//background.drawRect(1, 1, 118, 44, FlxColor.BLACK);
+	//background.drawRect(1, 46, 118, 73, FlxColor.BLACK);
+	background.screenCenter();
+    add(background);
+
+    choices = new Map();
+    choices[Fight] = new FlxText(background.x + 30, background.y + 48, 85, "Fight", 22);
+    choices[Defend] = new FlxText(background.x + 30, choices[Fight].y + choices[Fight].height + 8, 85, "Defend", 22);
+    choices[Heal] = new FlxText(background.x + 30, choices[Defend].y + choices[Defend].height + 8, 85, "Heal", 22);
+    choices[Flee] = new FlxText(background.x + 30, choices[Heal].y + choices[Heal].height + 8, 85, "Flee", 22);
+    add(choices[Fight]);
+    add(choices[Defend]);
+    add(choices[Heal]);
+    add(choices[Flee]);
+    
+    
+    //for(val in choice){
+    /*while(i< choice.length)
+    { 
+        choicetext= new FlxText(0, 0, 0, choice[i], 32);
+        choicetext.setFormat(null,32,FlxColor.RED);
+        this.setPosition(400 + (i*10), 500+ (i*10));   
+        i++;
+        
+       
+    } 
     //loadGraphic(AssetPaths.hudBackground__png);
     //this.setPosition(400, 500);
-            } 
-        }
-    }   
+       // }*/
+   }   
 }   
-/*
-    function AddChoices(){
-        for(val in choice){
-            while(i< choice.lengt for(val in choice){
-                while(i< choice.length){ 
-                    choicetext= new FlxText(0, 0, 0, choice[i], 32);
-                    choicetext.setFormat(null,32,FlxColor.RED,FlxTextAlign.CENTER);
-                    this.setPosition(400 + (i*10), 500+ (i*10));   
-                    i++;){ 
-                choicetext= new FlxText(0, 0, 0, choice[i], 32);
-                choicetext.setFormat(null,32,FlxColor.RED,FlxTextAlign.CENTER);
-                this.setPosition(400 + (i*10), 500+ (i*10));   
-                i++;
-            }
-    }
-    
-  }*/
-
--- BattleBro/source/PlayState.hx
+++ BattleBro/source/PlayState.hx
@@ -1,26 +1,32 @@
 package;
 
 import flixel.group.FlxGroup.FlxTypedGroup;
+import flash.geom.Matrix;
+import flash.geom.Point;
 import AssetPaths;
 import flixel.FlxG;
 import Enemy;
 import Player;
 //import Choice;
-import ChoiceHud;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 import flixel.FlxState;
 import flixel.text.FlxText;
+
+enum Choice
+{
+	Fight;
+    Defend;
+    Heal;
+    Flee;
+}
 
 class PlayState extends FlxState
 {
 	
 	var enemy:Enemy;
 	var player:Player;
-	var choiceHud:ChoiceHud;
-	//var choice:PlayersChoice;
 	private var background:FlxSprite;
-
 	public var choices:FlxTypedGroup<FlxText>;
 
 	override public function create()
@@ -28,9 +34,7 @@ class PlayState extends FlxState
 		//creates Enemy and Player objects 
 		enemy = new Enemy();
 		player = new Player();
-		choiceHud = new ChoiceHud();
-		//choiceTxt = new ChoiceHud();
-	//	choice = new PlayersChoice();
+		var choices:Map<Choice, FlxText>;
 		
 		super.create();
 		//creates background and sound
@@ -38,10 +42,20 @@ class PlayState extends FlxState
 		background.loadGraphic(AssetPaths.BackGround__jpg);
 		FlxG.sound.playMusic(AssetPaths.game_music__mp3);
 
+		choices = new Map();
+		choices[Fight] = new FlxText(background.x + 400, background.y + 450, 100, "Fight", 20);
+		choices[Defend] = new FlxText(background.x + 400, choices[Fight].y + choices[Fight].height + 8, 100, "Defend", 20);
+		choices[Heal] = new FlxText(background.x + 500, background.y + 450, 100, "Heal", 20);
+		choices[Flee] = new FlxText(background.x + 500, choices[Heal].y + choices[Heal].height + 8, 100, "Flee", 20);
+	
+			
 		add(background);
 		add(enemy);
 		add(player);
-		//add(choice);
+		add(choices[Fight]);
+		add(choices[Defend]);
+		add(choices[Heal]);
+		add(choices[Flee]);
 	}
 
 	override public function update(elapsed:Float)
